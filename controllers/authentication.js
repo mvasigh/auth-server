f36918aa9121d682a00013c303dd8f9a56c92083
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-10 09:15:29 -0500

    issue JWT on signup and authenticate with passport

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
+const config = require('../config');
 const User = require('../models/User');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   const { email, password } = req.body;
@@ -27,7 +34,7 @@ exports.signup = function(req, res, next) {
       if (err) {
         return next(err);
       }
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
---
+++ services/passport.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const User = require('../models/User');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Set up options for jwt strategy
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // See if User ID in the payload exists in our DB
+  User.findById(payload.sub, (err, user) => {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+  // If it does, call done with that user
+  // Otherwise, call done without a user object
+});
+
+// Tell passport to use this strategy
+passport.use(jwtLogin);
