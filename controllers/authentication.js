a47efe55320a74c561f22a44f59ab933825ec139
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-11 15:36:25 -0500

    add sign in route

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,6 +7,12 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next) {
+  // User has already had their email and password authed,
+  // just needs to be given a token
+  res.send({ token: tokenForUser(req.user) });
+};
+
 exports.signup = function(req, res, next) {
   const { email, password } = req.body;
 
